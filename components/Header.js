98d8444e4650288cfc673261543ec42a49a2f3ec
Mayurdeep Baruah <115894333+MayurdeepBaruah@users.noreply.github.com>
2023-08-08 00:06:23 +0530

    Initialize

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+function Header()
+{
+    return(
+        <div>
+            <div>
+                <h1>Form App</h1>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+export default Header;
---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header"
+function MyApp(){
+    return(
+        <div>
+            <Header></Header>
+        </div>
+    )
+}
+export default MyApp
